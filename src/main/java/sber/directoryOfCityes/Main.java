6e367b3843ce77e2f7a246ffe9e254a3d9770d3a
pipa <melatonin24@yandex.ru>
2022-09-01 15:26:03 +0300

    Parsing .csv

---
+++ src/main/java/sber/directoryOfCityes/Main.java
@@ -0,0 +1,52 @@
+package sber.directoryOfCityes;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<City> cities = parse();
+        printCities(cities);
+    }
+
+    public static List<City> parse() {
+        List<City> list = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(new FileInputStream("C:\\Users\\Евгений\\Desktop\\city_ru.csv"));
+            while (scanner.hasNextLine()) {
+                list.add(parse(scanner.nextLine()));
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    private static City parse(String line) {
+        Scanner scanner = new Scanner(line);
+        scanner.useDelimiter(";");
+        scanner.skip("\\d*");
+        String name = scanner.next();
+        String region = scanner.next();
+        String district = scanner.next();
+        int population = scanner.nextInt();
+        String foundation = null;
+        if (scanner.hasNext()) {
+            foundation = scanner.next();
+        }
+        scanner.close();
+
+        return new City(name, region, district, population, foundation);
+    }
+
+    private static void printCities(List<City> cities) {
+        for (City city : cities) {
+            System.out.println(city + "\n");
+        }
+    }
+}
