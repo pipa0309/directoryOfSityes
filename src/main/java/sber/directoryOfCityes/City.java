ecfbda2326bb0f6341e3d2ba39db7e0210326702
pipa <melatonin24@yandex.ru>
2022-09-01 15:35:35 +0300

    City class

---
+++ src/main/java/sber/directoryOfCityes/City.java
@@ -0,0 +1,27 @@
+package sber.directoryOfCityes;
+
+public class City {
+    private final String name;
+    private final String region;
+    private final String district;
+    private final Integer population;
+    private final String foundation;
+
+    public City(String name, String region, String district, Integer population, String foundation) {
+        this.name = name;
+        this.region = region;
+        this.district = district;
+        this.population = population;
+        this.foundation = foundation;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "name='" + name + '\'' +
+                ", region='" + region + '\'' +
+                ", district='" + district + '\'' +
+                ", population=" + population +
+                ", foundation='" + foundation + "'}";
+    }
+}
